cece8b7f0039ece7e0ef0508a2474b21ac3b53e7
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-13 17:32:53 -0800

    feat(2018 day-02): solution for day2 part 2

--- 2018/day-02/solution.js
+++ 2018/day-02/solution.js
@@ -8,4 +8,10 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
   let answer = boxes.getChecksum(data)
   console.log(`-- Part 1 --`)
   console.log(`Answer: ${answer}`)
+
+  let ids = boxes.getListFromData(data)
+  let similar = boxes.findSimilarIDs(ids)
+  let answer2 = boxes.getCommonLetters(similar[0], similar[1])
+  console.log(`-- Part 2 --`)
+  console.log(`Answer: ${answer2}`)
 })
