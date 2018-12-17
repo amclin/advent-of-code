e5679e65cf1d271bd574b9db195cc4e57e9467a5
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-17 00:51:37 -0800

    feat(2018 day-04): find the sleepiest minute of the most regularly asleep guard

--- 2018/day-04/solution.js
+++ 2018/day-04/solution.js
@@ -5,24 +5,44 @@ const {
   sortActivities
 } = require('./guards')
 const {
+  dynamicSort,
   loadInput,
-  parseLog
+  parseLog,
 } = require('./helpers')
 
 const init = (data) => {
   data = parseLog(data.join('\n'))
   data = sortActivities(data)
   data = processActivities(data)
-  const laziestGuard = findLaziestGuards(data)[0].id
+  let guardsBySleepTime = findLaziestGuards(data)
+  const laziestGuard = guardsBySleepTime[0].id
   const sleepiestTime = findSleepiestTimes(laziestGuard, data)[0].minute
-  console.log(findSleepiestTimes(laziestGuard, data))
+
   const answer = laziestGuard * sleepiestTime
-  const answer2 = undefined
+
+  // Sort the guards most likely to fall asleep on a repeated minute
+  guardsBySleepTime = guardsBySleepTime.map((guard) => {
+    if (guard.asleep > 0) {
+      let sleepiestTimes = findSleepiestTimes(guard.id, data)
+      guard.sleepiestMinute = sleepiestTimes[0].minute
+      guard.sleepiestMinuteQty = sleepiestTimes[0].qty
+    } else {
+      guard.sleepiestTime = 0
+      guard.sleepiestMinuteQty = 0
+    }
+    return guard
+  }).sort(dynamicSort('-sleepiestMinuteQty'))
+  const mostRegularGuard = guardsBySleepTime[0].id
+  const mostRegularGuardsMostRegularMinute = guardsBySleepTime[0].sleepiestMinute
+  const answer2 = mostRegularGuard * mostRegularGuardsMostRegularMinute
+
   console.log(`-- Part 1 --`)
   console.log(`Laziest Guard: ${laziestGuard}`)
   console.log(`Sleepiest time: ${sleepiestTime}`)
   console.log(`Answer: ${answer}`)
   console.log(`-- Part 2 --`)
+  console.log(`Most Regular Guard: ${mostRegularGuard}`)
+  console.log(`Most Regular Minute for same Guard: ${mostRegularGuardsMostRegularMinute}`)
   console.log(`Answer: ${answer2}`)
 }
 
