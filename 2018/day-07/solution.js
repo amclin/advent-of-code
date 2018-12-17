d370f9ecac69f68b2e974de6767d6c5ea00b6e59
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-17 13:56:26 -0800

    feat(2018 day-07): answer for part 1

--- 2018/day-07/solution.js
+++ 2018/day-07/solution.js
@@ -7,7 +7,7 @@ const {
 const init = (data) => {
   data = data.map(parseEntry)
   // const answer = sortInstructions(data).map((el) => el.id).join('')
-  const answer = sortInstructions(data)
+  const answer = sortInstructions(data).join('')
   console.log(`-- Part 1 --`)
   console.log(`Answer: ${answer}`)
   console.log(`-- Part 2 --`)
