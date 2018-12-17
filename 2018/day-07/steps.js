0ece5ca68ec524f40789dfc84d6d008154668ece
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-17 10:10:31 -0800

    test(2018 day-07): stub in unit tests for Day 7

---
+++ 2018/day-07/steps.js
@@ -0,0 +1,26 @@
+/**
+ * Parse the string into addressable data. Suitable for use in Array.prototype.map()
+ * @param {String} line from log file
+ * @param {Number} idx index of line in array
+ * @param {Array} arr read-only array provided by .map()
+ * @returns {Object} structured data
+ */
+const parseEntry = (line, idx, arr) => {
+  let entry = {}
+  return entry
+}
+
+/**
+ * Calculates the necessary order of entries based on their hierarchy
+ * @param {Array} entries List of instructions
+ * @returns {Array} sorted list
+ */
+const sortInstructions = (entries) => {
+  let sorted = ''
+  return sorted
+}
+
+module.exports = {
+  parseEntry,
+  sortInstructions
+}
---
+++ 2018/day-07/steps.test.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+const expect = require('chai').expect
+const {
+  parseEntry,
+  sortInstructions
+} = require('./steps')
+
+const testData = `Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.`.split('\n')
+
+describe('--- Day 7: The Sum of Its Parts ---', () => {
+  describe('Part 1:', () => {
+    describe('parseEntry', () => {
+      it.skip('Parses a line of the log to create a structured data object representing the entry', () => {
+        const test = testData[4]
+        const expected = {
+          id: 'E',
+          dep: 'B'
+        }
+        const actual = parseEntry(test)
+        expect(actual).to.equal(expected)
+      })
+    })
+    describe('sortInstructions()', () => {
+      it.skip('Puts the instructions in the necessary order', () => {
+        const expected = 'CABDFE'
+        let test = testData.map(parseEntry)
+        const actual = sortInstructions(test).map((el) => el.id).join('')
+        expect(actual).to.equal(expected)
+      })
+    })
+  })
+})
