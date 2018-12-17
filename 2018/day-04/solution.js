a7b34e50b6078390bcdc0b26f57710d0414c3096
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-17 00:08:26 -0800

    feat(2018 day-04): guard's sleepiest minute

--- 2018/day-04/solution.js
+++ 2018/day-04/solution.js
@@ -1,12 +1,28 @@
-const guards = require('./guards')
-const helpers = require('./helpers')
+const {
+  findLaziestGuards,
+  findSleepiestTimes,
+  processActivities,
+  sortActivities
+} = require('./guards')
+const {
+  loadInput,
+  parseLog
+} = require('./helpers')
 
-const data = helpers.loadInput((res) => helpers.parseLog())
-const laziestGuard = guards.getLaziestGuards(data)[0]
-const answer = guards.findSleepiestTimes(laziestGuard)
-const answer2 = undefined
+const init = (data) => {
+  data = parseLog(data.join('\n'))
+  data = sortActivities(data)
+  data = processActivities(data)
+  const laziestGuard = findLaziestGuards(data)[0].id
+  const sleepiestTime = findSleepiestTimes(laziestGuard, data)[0].minute
+  const answer = laziestGuard * sleepiestTime
+  const answer2 = undefined
+  console.log(`-- Part 1 --`)
+  console.log(`Laziest Guard: ${laziestGuard}`)
+  console.log(`Sleepiest time: ${sleepiestTime}`)
+  console.log(`Answer: ${answer}`)
+  console.log(`-- Part 2 --`)
+  console.log(`Answer: ${answer2}`)
+}
 
-console.log(`-- Part 1 --`)
-console.log(`Answer: ${answer}`)
-console.log(`-- Part 2 --`)
-console.log(`Answer: ${answer2}`)
+loadInput(init)
