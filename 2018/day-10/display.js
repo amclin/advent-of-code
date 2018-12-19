8808db641c0a81b76909a2869a101f968000e2f9
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-19 12:24:44 -0800

    feat(2018 day-10): render animation frame to a buffer

---
+++ 2018/day-10/display.js
@@ -0,0 +1,26 @@
+/**
+ * Renders a frame to page
+ * @param {Object} frame to render
+ * @param {Number} dim [w,h] dimensions of display
+  * @param {Array} origin [x,y] coordinates of origin
+ */
+const show = (frame, dim, origin) => {
+  let buffer = ''
+  for (let row = origin[1]; row < origin[1] + dim[1]; row++) {
+    for (let col = origin[0]; col < origin[0] + dim[0]; col++) {
+      let point = frame.contents.find((pt) => (col === pt.x && row === pt.y))
+      buffer += (typeof point !== 'undefined') ? '#' : '.'
+    }
+    buffer += '\n'
+  }
+  buffer = buffer.trim() // Remove exces whitespace
+  console.log(Array(dim[0]).fill('-').join('')) // border line
+  console.log(buffer)
+  console.log(Array(dim[0]).fill('-').join('')) // border line
+  console.log(`Focus ratio: ${frame.meta.focus}`) // Metadata
+  return buffer
+}
+
+module.exports = {
+  show
+}
---
+++ 2018/day-10/display.test.js
@@ -0,0 +1,71 @@
+/* eslint-env mocha */
+const expect = require('chai').expect
+const {
+  parseLine
+} = require('./helpers')
+const {
+  Beacon
+} = require('./beacons')
+const {
+  show
+} = require('./display')
+
+describe('--- Day 10: The Stars Align ---', () => {
+  describe('Display:', () => {
+    describe('show(frame, dimension, origin)', () => {
+      it('renders the frame to the display and returns it as a buffer', () => {
+        const expected = `......................
+......................
+......................
+......................
+......#...#..###......
+......#...#...#.......
+......#...#...#.......
+......#####...#.......
+......#...#...#.......
+......#...#...#.......
+......#...#...#.......
+......#...#..###......
+......................
+......................
+......................
+......................`
+        let testData = `position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>`.split('\n').map(parseLine)
+
+        const beaconTracker = new Beacon(testData)
+        const actual = show(beaconTracker.getFrame(3), [22, 16], [-6, -4])
+        expect(actual).to.equal(expected)
+      })
+    })
+  })
+})
