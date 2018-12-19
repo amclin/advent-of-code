f519b0b4cb0ab2559fc699aa1213b9be3caffa0b
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-19 12:58:58 -0800

    feat(2018 day-10): Automatically guess the best frame based on focus

--- 2018/day-10/solution.js
+++ 2018/day-10/solution.js
@@ -5,15 +5,33 @@ const {
 } = require('./beacons')
 
 const init = (data) => {
-  let beaconTracker = new Beacon(data)
+  const beaconTracker = new Beacon(data)
+  const start = 0
+  const end = 4
+
+  // Play the animation and interpret the frames
   for (let x = 0; x < 5; x++) {
     display.show(beaconTracker.getFrame(x), [22, 16], [-6, -4])
   }
 
-  // data = parseData(data)
+  // Find the frame with the best focus
+  const best = beaconTracker.frameMeta.reduce((acc, curr, idx) => {
+    return (curr.focus < acc.focus) ? {
+      idx: idx,
+      focus: curr.focus
+    } : acc
+  }, { idx: 0, focus: beaconTracker.frameMeta[0].focus })
+
   // const answer = sumMetadata(data)
   // const answer2 = data.value
-  // console.log(`-- Part 1 --`)
+  console.log(`-- Part 1 --`)
+  console.log(`Ran animation from frame ${start} to ${end}.`)
+  console.log(`The best frame appears to be ${best.idx} with a focus value of ${best.focus}`)
+  console.log(`Here's what the frame looks like:`)
+  console.log(`---------------------------------`)
+  display.show(beaconTracker.getFrame(best.idx), [22, 16], [-6, -4])
+  console.log(`---------------------------------`)
+
   // console.log(`Answer: ${answer}`)
   // console.log(`-- Part 2 --`)
   // console.log(`Answer: ${answer2}`)
