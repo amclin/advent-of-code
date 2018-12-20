427a5a9efad481aefb2ea48ad6a77c0c73a82597
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-19 16:12:20 -0800

    feat(2018 day-11): Compare across any size squares

--- 2018/day-11/solution.js
+++ 2018/day-11/solution.js
@@ -1,3 +1,4 @@
+const { dynamicSort } = require('../day-04/helpers')
 const { Rack } = require('./fuel-cells')
 const serial = 1133 // From puzzle input
 const size = [300, 300]
@@ -6,7 +7,22 @@ const squareSize = [3, 3]
 let powerBank = new Rack(serial, size)
 powerBank.tallySquares(squareSize)
 const answer = powerBank.getCellsByPower(squareSize)[0].coords
-const answer2 = ''
+
+const anySizeSquares = []
+for (let dial = 1; dial <= 5; dial++) {
+  powerBank.tallySquares([dial, dial])
+  let bestOfSizeX = powerBank.getCellsByPower(squareSize)[0]
+  anySizeSquares.push({
+    coords: bestOfSizeX.coords,
+    power: bestOfSizeX.squareTotal,
+    size: dial
+  })
+}
+
+const bestOfAnySize = anySizeSquares.sort(dynamicSort('-power'))[0]
+let answer2 = bestOfAnySize.coords
+answer2.push(bestOfAnySize.size)
+
 console.log(`-- Part 1 --`)
 console.log(`Answer: ${answer}`)
 console.log(`-- Part 2 --`)
