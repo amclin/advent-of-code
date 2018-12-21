375135bfcfea8fb4e0abfb3a9c136871713b75f0
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-21 14:35:13 -0800

    feat(2018 day-13): part 1 solution

--- 2018/day-13/solution.js
+++ 2018/day-13/solution.js
@@ -1,19 +1,23 @@
-const {
-  Track
-} = require('./tracks')
+const { loadInput } = require('./helpers')
+const { Track } = require('./tracks')
 
-const data = `/->-\\
-|   |  /----\\
-| /-+--+-\\  |
-| | |  | v  |
-\\-+-/  \\-+--/
-  \\------/   `
-
-const track = new Track(data)
-while (track.collision === false) {
-  track.advance()
+const init = (data) => {
+  const track = new Track(data)
+  while (track.collision === false) {
+    try {
+      track.advance()
+    } catch (err) {
+      console.error(`Reached a collision at ${track.collision.x},${track.collision.y} on frame ${track.frame}`)
+    }
+  }
+  const answer = [track.collision.x, track.collision.y]
+  // console.log(`Track state:`)
+  // console.log(track.display())
+  const answer2 = ''
+  console.log(`-- Part 1 --`)
+  console.log(`Answer: ${answer}`)
+  console.log(`-- Part 2 --`)
+  console.log(`Answer: ${answer2}`)
 }
 
-console.error(`Reached a collision at ${track.collision.x},${track.collision.y} on frame ${track.frame}`)
-console.log(`Track state:`)
-console.log(track.display())
+loadInput(init)
