ccffaa86594a8b8aac75422d637ae2bb790ca6a3
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-22 13:24:31 -0800

    test(2018 day-12): Try 500 generations

--- 2018/day-12/solution.js
+++ 2018/day-12/solution.js
@@ -10,7 +10,7 @@ const initialState = '#...#...##..####..##.####.#...#...#.#.#.#......##....#....
 
 const init = (data) => {
   const rules = data
-  const plantTracker = new Plants(initialState, rules)
+  let plantTracker = new Plants(initialState, rules)
   const generations = 20
   for (let gen = 1; gen <= generations; gen++) {
     plantTracker.advance()
@@ -18,6 +18,19 @@ const init = (data) => {
   console.log(`Generating ${generations} generations from the input looks like this:`)
   plantTracker.display()
   const answer = plantTracker.getCheckSum(generations)
+
+  // Start for part 2
+  const generations2 = 500
+  for (let gen = generations + 1; gen <= generations2; gen++) {
+    plantTracker.advance()
+  }
+  console.log(`Generating ${generations2} generations from the input looks like this:`)
+  plantTracker.display()
+
+  // 500 generations takes about 30s, so running 50B generations isn't possible. It's
+  // clear looking at the log there's a "Game of Life"-style glider.
+  // See output/500-generations.txt and output/500-generations.png
+
   const answer2 = ''
 
   console.log(`-- Part 1 --`)
