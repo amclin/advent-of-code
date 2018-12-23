59f4ea4a7c5b073ac0b05bb6234d5b2f4f76ac28
Anthony McLin <anthony.mclin@rosetta.com>
2018-12-23 10:27:29 -0800

    refactor(2018 day-14): remove extraneous code leftover from debugging day 14

--- 2018/day-14/solution.js
+++ 2018/day-14/solution.js
@@ -1,23 +1,15 @@
 const {
   calculateXAfterY,
   findPattern,
-  loopRecipesForElves,
   Recipes
 } = require('./recipes')
 
 const input = 540561
 
 let recipes = new Recipes([3, 7])
-
 const answer = calculateXAfterY(10, input, recipes)
-
 console.log(`-- Part 1 --`)
 console.log(`Answer: ${answer}`)
-
-recipes = new Recipes([3, 7])
-while (recipes.length < 3000) {
-  loopRecipesForElves(recipes, 1)
-}
 
 recipes = new Recipes([3, 7])
 const bufferSize = 10000
