fd6fb5192206d3895d6361124b0ada3479ec40c3
Anthony McLin <git@anthonymclin.com>
2019-12-06 00:26:39 -0600

    test(2019-day-01): specs for recursive fuel calculations

--- 2019/day-01/part-1/fuel-calculator.test.js
+++ 2019/day-01/part-1/fuel-calculator.test.js
@@ -1,15 +1,35 @@
 /* eslint-env mocha */
 const expect = require('chai').expect
-const { calculateFuel } = require('./fuel-calculator')
+const { calculateFuel, calculateFuelRecursively } = require('./fuel-calculator')
 
 describe('--- 2019 Day 1: The Tyranny of the Rocket Equation ---', () => {
   describe('Part 1', () => {
-    it('calculates the fuel amount for a specified mass', () => {
-      const inputs = [12, 14, 1969, 100756]
-      const outputs = [2, 2, 654, 33583]
+    describe('calculateFuel()', () => {
+      it('calculates the fuel amount for a specified mass', () => {
+        const inputs = [12, 14, 1969, 100756]
+        const outputs = [2, 2, 654, 33583]
 
-      inputs.forEach((input, idx) => {
-        expect(calculateFuel(input)).equals(outputs[idx])
+        inputs.forEach((input, idx) => {
+          expect(calculateFuel(input)).equals(outputs[idx])
+        })
+      })
+      it('will not output negative quantities for fuel', () => {
+        const inputs = [0, 1, -2345]
+        const outputs = [0, 0, 0]
+
+        inputs.forEach((input, idx) => {
+          expect(calculateFuel(input)).equals(outputs[idx])
+        })
+      })
+    })
+    describe('calculateFuelRecursively()', () => {
+      it('calculates fuel amount for a specified mass, along with the fuel for the fuel', () => {
+        const inputs = [14, 1969, 100756]
+        const outputs = [2, 966, 50346]
+
+        inputs.forEach((input, idx) => {
+          expect(calculateFuelRecursively(input)).equals(outputs[idx])
+        })
       })
     })
   })
