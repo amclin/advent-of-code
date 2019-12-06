a5650947ee061e50f0d51a94754ece150f9ed551
Anthony McLin <git@anthonymclin.com>
2019-12-06 12:49:55 -0800

    test(2019-day-02): specs for running an IntCode program

--- 2019/day-02/intcodeParser.test.js
+++ 2019/day-02/intcodeParser.test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 const expect = require('chai').expect
-const { step } = require('./intcodeParser')
+const { step, runProgram } = require('./intcodeParser')
 
 describe('--- 2019 Day 2: 1202 Program Alarm ---', () => {
   describe('Part 1', () => {
@@ -23,6 +23,26 @@ describe('--- 2019 Day 2: 1202 Program Alarm ---', () => {
           const data = [oppcode, 5, 6, 3, 99, 2, 3]
           step({ position: 0, data })
           expect(data[3]).equals(3)
+        })
+      })
+    })
+    describe('runProgram()', () => {
+      it('runs through sequential steps of an intCode program', () => {
+        const testInputs = [
+          [1, 0, 0, 0, 99],
+          [2, 3, 0, 3, 99],
+          [2, 4, 4, 5, 99, 0],
+          [1, 1, 1, 4, 99, 5, 6, 0, 99]
+        ]
+        const testOutputs = [
+          [2, 0, 0, 0, 99],
+          [2, 3, 0, 6, 99],
+          [2, 4, 4, 5, 99, 9801],
+          [30, 1, 1, 4, 2, 5, 6, 0, 99]
+        ]
+        testInputs.forEach((data, idx) => {
+          runProgram(data)
+          expect(data).to.deep.equal(testOutputs[idx])
         })
       })
     })
