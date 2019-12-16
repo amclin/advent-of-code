aa83c0bc4ad03f70c27c3dc45d8c4354b02f782a
Anthony McLin <git@anthonymclin.com>
2019-12-16 10:43:01 -0800

    test(20190-day-03): specs for finding closest wire intersection by wire distance

--- 2019/day-03/wires.test.js
+++ 2019/day-03/wires.test.js
@@ -55,5 +55,37 @@ describe('--- 2019 Day 3: Crossed Wires ---', () => {
         expect(getClosesetIntersection(findWireIntersections(wires)).distance).to.equal(distances[idx])
       })
     })
+    describe('getIntersectionWireDistance()', () => {
+      it('calculates the summed total wire length to reach the specified intersection', () => {
+        const wires = [
+          'R8,U5,L5,D3',
+          'U7,R6,D4,L4'
+        ]
+        const expected = [
+          40, 30
+        ]
+        const intersections = findWireIntersections(...wires)
+        const actual = intersections.map((inter) => getIntersectionWireDistance({wires, intersection:inter}))
+        expect(actual).to.deep.equal(expected)
+      })
+    })
+    describe('getClosestIntersectionByWire()', () => {
+      it('can be used to find the wire distance to the closest intersection', () => {
+        const wiresets = [
+          [
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83'
+          ], [
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+          ]
+        ]
+        const distances = [610, 410]
+        wiresets.forEach((wires, idx) => {
+          const intersections = findWireIntersections(wires)
+          expect(getClosesetIntersectionByWire({ intersections, wires }).distance).to.equal(distances[idx])
+        })
+      })
+    })
   })
 })
