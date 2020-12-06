e15993af157b3d7c79e997d16ee9b1ddddd4e751
Anthony McLin <git@anthonymclin.com>
2020-12-06 15:04:42 -0800

    feat[2020-day-01]: find 3 expenses totaling 2020

--- 2020/day-01/expenseValidation.test.js
+++ 2020/day-01/expenseValidation.test.js
@@ -1,6 +1,15 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
 const { validateRecords } = require('./expenseValidation')
+
+/**
+ * Sum all the values in an array
+ */
+const arrSum = (arr) => arr.reduce((x, y) => x + y, 0)
+/**
+ * Multiply all the values in an array
+ */
+const arrMult = (arr) => arr.reduce((x, y) => x * y, 1)
 const testData = [
   1721,
   979,
@@ -15,26 +24,19 @@ describe('--- 2020 Day 1: Report Repair ---', () => {
     describe('validateRecords()', () => {
       it('it finds the two records that sum to 2020', () => {
         const expected = [1721, 299]
-        const results = validateRecords(testData, 2020)
+        const results = validateRecords(testData, undefined, 2)
         // Should be 2 results
         expect(results.length).to.equal(2)
         // Result order is unnecessary, but all expected hould be in the result set
         expected.forEach(result => {
           expect(testData.indexOf(result)).to.be.greaterThan(-1)
         })
-      })
-      it('it can find a specified number of records adding up to 2020', () => {
-        const expected = [979, 366, 675]
-        const results = validateRecords(testData, undefined, 3)
-        // Should same number of results
-        expect(results.length).to.equal(expected.length)
-        // Result order is unnecessary, but all expected hould be in the result set
-        expected.forEach(result => {
-          expect(testData.indexOf(result)).to.be.greaterThan(-1)
-        })
+        // Results add up to the checksum
+        expect(arrSum(results)).to.equal(2020)
+        // Confirm the multiplied total
+        expect(arrMult(results)).to.equal(514579)
       })
       it('it supports specifying an alternate checksum', () => {
-        const arrSum = (arr) => arr.reduce((x, y) => x + y, 0)
         const expected = [testData[3], testData[5]]
         const checksum = arrSum(expected)
         const results = validateRecords(testData, checksum)
@@ -52,6 +54,24 @@ describe('--- 2020 Day 1: Report Repair ---', () => {
       })
       it('Throws an error when no records match checksum', () => {
         expect(() => validateRecords([1, 2, 3], 100)).to.throw('Couldn\'t find a checksum match')
+      })
+    })
+  })
+  describe('Part 2', () => {
+    describe('validateRecords()', () => {
+      it('it can find a specified number of records adding up to 2020', () => {
+        const expected = [979, 366, 675]
+        const results = validateRecords(testData, undefined, 3)
+        // Should same number of results
+        expect(results.length).to.equal(expected.length)
+        // Result order is unnecessary, but all expected hould be in the result set
+        expected.forEach(result => {
+          expect(testData.indexOf(result)).to.be.greaterThan(-1)
+        })
+        // Results add up to the checksum
+        expect(arrSum(results)).to.equal(2020)
+        // Confirm the multiplied total
+        expect(arrMult(results)).to.equal(241861950)
       })
     })
   })
--- 2020/day-01/solution.js
+++ 2020/day-01/solution.js
@@ -17,47 +17,18 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
 
   const part1 = () => {
     const data = resetInput()
-    const results = validateRecords(data)
-
-    return results[0] * results[1]
+    return validateRecords(data) // Find 2 results for 2020
+      .reduce((total, res) => total * res, 1)
   }
 
-  // const part2 = ({ target, maxNoun, maxVerb }) => {
-  //   // Helper for running the program with specified noun and verb inputs
-  //   const tryProgram = ({
-  //     noun,
-  //     verb
-  //   }) => {
-  //     const data = resetInput()
-  //     data[1] = noun
-  //     data[2] = verb
-  //     runProgram({ data })
-  //     console.debug(`Running with noun:${noun} and verb:${verb} produces ${data[0]}`)
-  //     return Number(data[0])
-  //   }
-
-  //   // Manipulate and loop through attempts for Part 2
-  //   let noun = -1
-  //   while (noun <= maxNoun) {
-  //     let verb = -1
-  //     noun++
-  //     while (verb <= maxVerb) {
-  //       verb++
-  //       const output = tryProgram({
-  //         noun,
-  //         verb
-  //       })
-  //       // Break the search loop on success
-  //       if (output === target) {
-  //         return 100 * noun + verb
-  //       }
-  //     }
-  //   }
-  // }
-
+  const part2 = () => {
+    const data = resetInput()
+    return validateRecords(data, undefined, 3) // Find 3 results for 2020
+      .reduce((total, res) => total * res, 1)
+  }
   const answers = []
   answers.push(part1())
-  // answers.push(part2())
+  answers.push(part2())
 
   answers.forEach((ans, idx) => {
     console.info(`-- Part ${idx + 1} --`)
