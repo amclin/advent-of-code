506474dae9611f481580a5adfed0f77acfa9cf7b
Anthony McLin <git@anthonymclin.com>
2020-12-06 11:56:12 -0800

    test[2020-day-01]: unit tests for expense validation process

---
+++ 2020/day-01/expenseValidation.test.js
@@ -0,0 +1,48 @@
+/* eslint-env mocha */
+const { expect } = require('chai')
+const { validateRecords } = require('./expenseValidation')
+const testData = [
+  1721,
+  979,
+  366,
+  299,
+  675,
+  1456
+]
+
+describe('--- 2020 Day 1: Report Repair ---', () => {
+  describe('Part 1', () => {
+    describe('validateRecords()', () => {
+      it('it finds the two records that sum to 2020', () => {
+        const expected = [1721, 299]
+        const results = validateRecords(testData, 2020)
+        // Should be 2 results
+        expect(results.length).to.equal(2)
+        // Result order is unnecessary, but all expected hould be in the result set
+        expected.forEach(result => {
+          expect(testData.indexOf(result)).to.be.greaterThan(-1)
+        })
+      })
+      it('it supports specifying an alternate checksum', () => {
+        const arrSum = (arr) => arr.reduce((x, y) => x + y, 0)
+        const expected = [testData[3], testData[5]]
+        const checksum = arrSum(expected)
+        const results = validateRecords(testData, checksum)
+        // Should be 2 results
+        expect(results.length).to.equal(2)
+        // Result order is unnecessary, but all expected hould be in the result set
+        expected.forEach(result => {
+          expect(results.indexOf(result)).to.be.greaterThan(-1)
+        })
+        // Results add up to the checksum
+        expect(arrSum(results)).to.equal(checksum)
+      })
+      it('Throws an error when no records provided', () => {
+        expect(validateRecords).to.throw()
+      })
+      it('Throws an error when no records match checksum', () => {
+        expect(() => validateRecords([1, 2, 3], 100)).to.throw('Couldn\'t find a checksum match')
+      })
+    })
+  })
+})
