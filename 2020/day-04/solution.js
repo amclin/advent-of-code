2a050ca31f31d003eee42f1cdf86c55c073fdb16
Anthony McLin <git@anthonymclin.com>
2020-12-06 23:26:34 -0800

    feat(2020-day-04): process a stream of passports to count valid ones
    Solution for part 1

--- 2020/day-04/solution.js
+++ 2020/day-04/solution.js
@@ -1,35 +1,65 @@
 const fs = require('fs')
 const path = require('path')
+const split2 = require('split2')
 const filePath = path.join(__dirname, 'input.txt')
-const { inputToArray } = require('../../2018/inputParser')
+const { parseScan, validate } = require('./passports')
 
-fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
-  if (err) throw err
+const answers = []
 
-  initData = inputToArray(initData.trim())
+const part1 = () => {
+  let recordBuffer = ''
+  let validCount = 0
+  let invalidCount = 0
+  let totalCount = 0
 
-  const resetInput = () => {
-    // Deep copy to ensure we aren't mutating the original data
-    return JSON.parse(JSON.stringify(initData))
+  const processBuffer = () => {
+    // Handle the record in the buffer
+    const passport = parseScan(recordBuffer)
+    try {
+      if (validate(passport)) {
+        validCount++
+      }
+    } catch (e) {
+      invalidCount++
+      console.warn(e.message)
+      console.debug('Invalid passport', passport)
+    }
+    totalCount++
   }
 
-  const part1 = () => {
-    const data = resetInput()
-    console.debug(data)
-    return 'No answer yet'
-  }
+  fs.createReadStream(filePath)
+    .pipe(split2())
+    .on('data', (data) => {
+      // Emmpty line indicates record separator
+      if (data.trim() === '') {
+        processBuffer()
+        // flush buffer
+        recordBuffer = ''
+        return
+      }
+      // Add the line to the buffer
+      recordBuffer += data.trim() + '\n'
+    })
+    .on('end', () => {
+      // get the final record from the buffer
+      processBuffer()
+      console.info('Total passports found:', totalCount)
+      console.info('Invalid passports', invalidCount)
+      answers.push(validCount)
+      answers.push(part2())
 
-  const part2 = () => {
-    const data = resetInput()
-    console.debug(data)
-    return 'No answer yet'
-  }
-  const answers = []
-  answers.push(part1())
-  answers.push(part2())
+      answers.forEach((ans, idx) => {
+        console.info(`-- Part ${idx + 1} --`)
+        console.info(`Answer: ${ans}`)
+      })
+    })
 
-  answers.forEach((ans, idx) => {
-    console.info(`-- Part ${idx + 1} --`)
-    console.info(`Answer: ${ans}`)
-  })
-})
+  console.debug('Invalid passports:', invalidCount)
+  return validCount
+}
+
+const part2 = () => {
+  return 'No answer yet'
+}
+
+part1()
