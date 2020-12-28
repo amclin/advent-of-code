5dd4d8984de651733dfe5ce1f67444a0c91a9362
Anthony McLin <git@anthonymclin.com>
2020-12-27 17:54:59 -0800

    feat(2020-day-13): find the next bus to the airport

---
+++ 2020/day-13/busSchedules.js
@@ -0,0 +1,26 @@
+const parseSchedule = (schedule) => {
+  // Drop "x" and convert to numeric
+  return schedule.split(',').filter((e) => e !== 'x').map(Number)
+}
+
+const findNext = ({ time, schedule }) => {
+  const result = schedule.map((route) => {
+    return {
+      route,
+      wait: route - (time % route)
+    }
+  }).sort(
+    (a, b) => a.wait - b.wait
+  )
+
+  result.forEach((res) => {
+    console.debug(`Wait for route ${res.route} is ${res.wait}`)
+  })
+
+  return result[0]
+}
+
+module.exports = {
+  parseSchedule,
+  findNext
+}
---
+++ 2020/day-13/busSchedules.test.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+const { expect } = require('chai')
+const { parseSchedule, findNext } = require('./busSchedules')
+
+const testData = {
+  time: 939,
+  schedule: '7,13,x,x,59,x,31,19'
+}
+
+describe.only('--- Day 13: Shuttle Search ---', () => {
+  describe('Part 1', () => {
+    describe('parseSchedule()', () => {
+      it('cleans up the schedule', () => {
+        expect(parseSchedule(testData.schedule))
+          .to.deep.equal([7, 13, 59, 31, 19])
+      })
+    })
+    describe('findNext()', () => {
+      it('finds the next departing bus', () => {
+        expect(findNext({
+          time: 939,
+          schedule: parseSchedule(testData.schedule)
+        })).to.deep.equal({
+          route: 59,
+          wait: 5
+        })
+      })
+    })
+  })
+})
