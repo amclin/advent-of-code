7f8a13f2706ed58424e8600cc86f18d762d493b5
Anthony McLin <git@anthonymclin.com>
2021-12-13 22:34:52 -0800

    feat(2021-day-04): find the first board with a bingo

---
+++ 2021/day-04/solution.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join(__dirname, 'input.txt')
+const { linesToArray, parseData } = require('../../2018/inputParser')
+const { markBoard, checkWinner, scoreBoard } = require('./bingo')
+
+fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
+  if (err) throw err
+
+  initData = linesToArray(initData.trim())
+
+  const resetInput = () => {
+    // Deep copy to ensure we aren't mutating the original data
+    const data = JSON.parse(JSON.stringify(initData))
+
+    // split up data
+    const draws = parseData(data.shift())
+    const boards = []
+    for (let x = 0; x < data.length; x = x + 5) {
+      boards.push(
+        data.slice(x, x + 5).map(parseData)
+      )
+    }
+
+    return {
+      boards,
+      draws
+    }
+  }
+
+  const part1 = () => {
+    const data = resetInput()
+
+    let winner = false
+    let draw = -1
+    while (winner === false && draw < data.draws.length) {
+      // next draw
+      draw++
+
+      // Mark each board that has the number
+      console.debug(`Checking draw ${data.draws[draw]}`)
+      data.boards = data.boards.map((board) => {
+        return markBoard(board, data.draws[draw])
+      })
+
+      // Check for a winner
+      data.boards.forEach((board, idx) => {
+        if (checkWinner(board) === 'winner') {
+          console.debug(`Board ${idx} is the winner`)
+          console.debug(data.boards[idx])
+          winner = idx
+        }
+      })
+    }
+
+    // winner found
+    console.debug(`Score is ${scoreBoard(data.boards[winner])}`)
+    return scoreBoard(data.boards[winner]) * data.draws[draw]
+  }
+
+  const part2 = () => {
+    // const data = resetInput()
+    // console.debug(data)
+    return 'No answer yet'
+  }
+  const answers = []
+  answers.push(part1())
+  answers.push(part2())
+
+  answers.forEach((ans, idx) => {
+    console.info(`-- Part ${idx + 1} --`)
+    console.info(`Answer: ${ans}`)
+  })
+})
