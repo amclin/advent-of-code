8ec8e80282e97f509f43d089fbda25f16227b254
Anthony McLin <git@anthonymclin.com>
2021-12-16 15:34:10 -0800

    feat(2021-day-09): find the largest basins
    Solves part 2
    
    I was naughty and didn't follow TDD for this one. Mistakenly thought
    the logic would be more terse than it ended up being.

--- 2021/day-09/solution.js
+++ 2021/day-09/solution.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const filePath = path.join(__dirname, 'input.txt')
 const { linesToArray } = require('../../2018/inputParser')
-const { findLocalLows } = require('./basins')
+const { findLocalLows, flow } = require('./basins')
 
 fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
   if (err) throw err
@@ -24,8 +24,24 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
 
   const part2 = () => {
     const data = resetInput()
-    // console.debug(data)
-    return 'No answer yet'
+    let map = {
+      coords: [...Array(data.length)].map(() => Array(data[0].length)), // allocate a map
+      basins: [0] // fake first basin with no area to avoid having to figure out JS loose-type false/truthy and <> bullshit
+    }
+
+    for (let x = 0; x < data[0].length; x++) {
+      for (let y = 0; y < data.length; y++) {
+        console.debug(`Starting flow trace at ${x},${y}`)
+        map = flow(x, y, map, data, 9).map // 9 is a magic number for assuming we start with fresh drains
+      }
+    }
+
+    console.debug(`Found ${map.basins.length} basins.`)
+
+    // Multiply the area of the 3 largest basins
+    return map.basins.sort((a, b) => a - b)
+      .slice(map.basins.length - 3)
+      .reduce((a, b) => a * b)
   }
   const answers = []
   answers.push(part1())
