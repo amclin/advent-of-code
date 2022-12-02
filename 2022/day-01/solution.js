75259c8cbdb82528e0a730de8882a6805e749db6
Anthony McLin <git@anthonymclin.com>
2022-12-01 22:24:06 -0800

    feat(2022-day-01): solution for part two, getting the top 3 elves' calories

--- 2022/day-01/solution.js
+++ 2022/day-01/solution.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const { findElfWithMost, parseCalorieData } = require('./calories')
+const { findElfWithMost, parseCalorieData, sortElvesByCalories } = require('./calories')
 const filePath = path.join(__dirname, 'input.txt')
 // const { inputToArray } = require('../../2018/inputParser')
 
@@ -11,19 +11,23 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, initData) => {
 
   const resetInput = () => {
     // Deep copy to ensure we aren't mutating the original data
-    return JSON.parse(JSON.stringify(initData))
+    return parseCalorieData(JSON.parse(JSON.stringify(initData)))
   }
 
   const part1 = () => {
     const data = resetInput()
-    const elfWithMost = findElfWithMost(parseCalorieData(data))
+    const elfWithMost = findElfWithMost(data)
     return elfWithMost.reduce((a, b) => a + b) // Tally calories for elf with most
   }
 
   const part2 = () => {
     const data = resetInput()
-    console.debug(data)
-    return 'No answer yet'
+    const elves = sortElvesByCalories(data)
+    let results = 0
+    for (let x = 0; x < 3; x++) { // iterate through the top 3 results
+      results += elves[x].reduce((a, b) => a + b, 0) // sum the calorie counts
+    }
+    return results
   }
   const answers = []
   answers.push(part1())
